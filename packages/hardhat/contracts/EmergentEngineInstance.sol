3074e2c8adbfd985d23ec4d1fd4e35144163fabb
cryptonautics <cryptonautics42@gmail.com>
2022-02-06 16:47:30 -0500

    Changed storage state to four private copies of each datastructure and added pre-OZ4.X versions of collection data structures

--- packages/hardhat/contracts/EmergentEngineInstance.sol
+++ packages/hardhat/contracts/EmergentEngineInstance.sol
@@ -62,28 +62,7 @@ contract EmergentEngineInstance is Ownable, Pausable, ReentrancyGuard {
   EnumerableSet.AddressSet private betaCryptonauts;
   EnumerableSet.AddressSet private gammaCryptonauts;
   EnumerableSet.AddressSet private deltaCryptonauts;
-  EnumerableSet.AddressSet private epsilonCryptonauts;
-  EnumerableSet.AddressSet private zetaCryptonauts;
-  EnumerableSet.AddressSet private etaCryptonauts;
-  EnumerableSet.AddressSet private thetaCryptonauts;
-  EnumerableSet.AddressSet private iotaCryptonauts;
-  EnumerableSet.AddressSet private kappaCryptonauts;
-  EnumerableSet.AddressSet private lambdaCryptonauts;
-  EnumerableSet.AddressSet private muCryptonauts;
-
-  EnumerableSet.AddressSet private nuCryptonauts;
-  EnumerableSet.AddressSet private xiCryptonauts;
-  EnumerableSet.AddressSet private omicronCryptonauts;
-  EnumerableSet.AddressSet private piCryptonauts;
-  EnumerableSet.AddressSet private rhoCryptonauts;
-  EnumerableSet.AddressSet private sigmaCryptonauts;
-  EnumerableSet.AddressSet private tauCryptonauts;
-  EnumerableSet.AddressSet private upsilonCryptonauts;
-  EnumerableSet.AddressSet private phiCryptonauts;
-  EnumerableSet.AddressSet private chiCryptonauts;
-  EnumerableSet.AddressSet private psiCryptonauts;
-  EnumerableSet.AddressSet private omegaCryptonauts;
-
+  
   //
   // 'Transformations' are uint to address maps
   //
@@ -92,195 +71,90 @@ contract EmergentEngineInstance is Ownable, Pausable, ReentrancyGuard {
   EnumerableMap.UintToAddressMap private betaTransformation;
   EnumerableMap.UintToAddressMap private gammaTransformation;
   EnumerableMap.UintToAddressMap private deltaTransformation;
-  EnumerableMap.UintToAddressMap private epsilonTransformation;
-  EnumerableMap.UintToAddressMap private zetaTransformation;
-  EnumerableMap.UintToAddressMap private etaTransformation;
-  EnumerableMap.UintToAddressMap private thetaTransformation;
-  EnumerableMap.UintToAddressMap private iotaTransformation;
-  EnumerableMap.UintToAddressMap private kappaTransformation;
-  EnumerableMap.UintToAddressMap private lambdaTransformation;
-  EnumerableMap.UintToAddressMap private muTransformation;
-
-  EnumerableMap.UintToAddressMap private nuTransformation;
-  EnumerableMap.UintToAddressMap private xiTransformation;
-  EnumerableMap.UintToAddressMap private omicronTransformation;
-  EnumerableMap.UintToAddressMap private piTransformation;
-  EnumerableMap.UintToAddressMap private rhoTransformation;
-  EnumerableMap.UintToAddressMap private sigmaTransformation;
-  EnumerableMap.UintToAddressMap private tauTransformation;
-  EnumerableMap.UintToAddressMap private upsilonTransformation;
-  EnumerableMap.UintToAddressMap private phiTransformation;
-  EnumerableMap.UintToAddressMap private chiTransformation;
-  EnumerableMap.UintToAddressMap private psiTransformation;
-  EnumerableMap.UintToAddressMap private omegaTransformation;
 
   //
   // 'Posinaught' are unsigned integers
   //
 
-  uint256 public alphaPosinaught;
-  uint256 public betaPosinaught;
-  uint256 public gammaPosinaught;
-  uint256 public deltaPosinaught;
-  uint256 public epsilonPosinaught;
-  uint256 public zetaPosinaught;
-  uint256 public etaPosinaught;
-  uint256 public thetaPosinaught;
-  uint256 public iotaPosinaught;
-  uint256 public kappaPosinaught;
-  uint256 public lambdaPosinaught;
-  uint256 public muPosinaught;
-
-  uint256 private nuPosinaught;
-  uint256 private xiPosinaught;
-  uint256 private omicronPosinaught;
-  uint256 private piPosinaught;
-  uint256 private rhoPosinaught;
-  uint256 private sigmaPosinaught;
-  uint256 private tauPosinaught;
-  uint256 private upsilonPosinaught;
-  uint256 private phiPosinaught;
-  uint256 private chiPosinaught;
-  uint256 private psiPosinaught;
-  uint256 private omegaPosinaught;
-
+  uint256 private alphaPosinaught;
+  uint256 private betaPosinaught;
+  uint256 private gammaPosinaught;
+  uint256 private deltaPosinaught;
+  
   //
   // 'Omninaught' are signed integers
   //
 
-  int256 public alphaOmninaught;
-  int256 public betaOmninaught;
-  int256 public gammaOmninaught;
-  int256 public deltaOmninaught;
-  int256 public epsilonOmninaught;
-  int256 public zetaOmninaught;
-  int256 public etaOmninaught;
-  int256 public thetaOmninaught;
-  int256 public iotaOmninaught;
-  int256 public kappaOmninaught;
-  int256 public lambdaOmninaught;
-  int256 public muOmninaught;
-
-  int256 private nuOmninaught;
-  int256 private xiOmninaught;
-  int256 private omicronOmninaught;
-  int256 private piOmninaught;
-  int256 private rhoOmninaught;
-  int256 private sigmaOmninaught;
-  int256 private tauOmninaught;
-  int256 private upsilonOmninaught;
-  int256 private phiOmninaught;
-  int256 private chiOmninaught;
-  int256 private psiOmninaught;
-  int256 private omegaOmninaught;
+  int256 private alphaOmninaught;
+  int256 private betaOmninaught;
+  int256 private gammaOmninaught;
+  int256 private deltaOmninaught;
   
   //
   // 'Lexiqueue' are strings
   //
 
-  string public alphaLexiqueue;
-  string public betaLexiqueue;
-  string public gammaLexiqueue;
-  string public deltaLexiqueue;
-  string public epsilonLexiqueue;
-  string public zetaLexiqueue;
-  string public etaLexiqueue;
-  string public thetaLexiqueue;
-  string public iotaLexiqueue;
-  string public kappaLexiqueue;
-  string public lambdaLexiqueue;
-  string public muLexiqueue;
-
-  string private nuLexiqueue;
-  string private xiLexiqueue;
-  string private omicronLexiqueue;
-  string private piLexiqueue;
-  string private rhoLexiqueue;
-  string private sigmaLexiqueue;
-  string private tauLexiqueue;
-  string private upsilonLexiqueue;
-  string private phiLexiqueue;
-  string private chiLexiqueue;
-  string private psiLexiqueue;
-  string private omegaLexiqueue;
-
+  string private alphaLexiqueue;
+  string private betaLexiqueue;
+  string private gammaLexiqueue;
+  string private deltaLexiqueue;
+  
   //
   // 'Cryptoqueue' are address arrays
   //
 
-  address[] public alphaCryptoqueue;
-  address[] public betaCryptoqueue;
-  address[] public gammaCryptoqueue;
-  address[] public deltaCryptoqueue;
-  address[] public epsilonCryptoqueue;
-  address[] public zetaCryptoqueue;
-  address[] public etaCryptoqueue;
-  address[] public thetaCryptoqueue;
-  address[] public iotaCryptoqueue;
-  address[] public kappaCryptoqueue;
-  address[] public lambdaCryptoqueue;
-  address[] public muCryptoqueue;
-
-  address[] private nuCryptoqueue;
-  address[] private xiCryptoqueue;
-  address[] private omicronCryptoqueue;
-  address[] private piCryptoqueue;
-  address[] private rhoCryptoqueue;
-  address[] private sigmaCryptoqueue;
-  address[] private tauCryptoqueue;
-  address[] private upsilonCryptoqueue;
-  address[] private phiCryptoqueue;
-  address[] private chiCryptoqueue;
-  address[] private psiCryptoqueue;
-  address[] private omegaCryptoqueue;
+  address[] private alphaCryptoqueue;
+  address[] private betaCryptoqueue;
+  address[] private gammaCryptoqueue;
+  address[] private deltaCryptoqueue;
 
   //
   // 'Posinaughtiqueue' are arrays of unsigned integers
   //
 
-  uint256[] public alphaPosinaughtiqueue;
-  uint256[] public betaPosinaughtiqueue;
-  uint256[] public gammaPosinaughtiqueue;
-  uint256[] public deltaPosinaughtiqueue;
-  uint256[] public epsilonPosinaughtiqueue;
-  uint256[] public zetaPosinaughtiqueue;
-  uint256[] public etaPosinaughtiqueue;
-  uint256[] public thetaPosinaughtiqueue;
-  uint256[] public iotaPosinaughtiqueue;
-  uint256[] public kappaPosinaughtiqueue;
-  uint256[] public lambdaPosinaughtiqueue;
-  uint256[] public muPosinaughtiqueue;
+  uint256[] private alphaPosinaughtiqueue;
+  uint256[] private betaPosinaughtiqueue;
+  uint256[] private gammaPosinaughtiqueue;
+  uint256[] private deltaPosinaughtiqueue;
 
-  uint256[] private nuPosinaughtiqueue;
-  uint256[] private xiPosinaughtiqueue;
-  uint256[] private omicronPosinaughtiqueue;
-  uint256[] private piPosinaughtiqueue;
-  uint256[] private rhoPosinaughtiqueue;
-  uint256[] private sigmaPosinaughtiqueue;
-  uint256[] private tauPosinaughtiqueue;
-  uint256[] private upsilonPosinaughtiqueue;
-  uint256[] private phiPosinaughtiqueue;
-  uint256[] private chiPosinaughtiqueue;
-  uint256[] private psiPosinaughtiqueue;
-  uint256[] private omegaPosinaughtiqueue;
+  //
+  // 'Lexiqueuetwo' are arrays of strings
+  //
 
+  string[] private alphaLexiqueuetwo;
+  string[] private betaLexiqueuetwo;
+  string[] private gammaLexiqueuetwo;
+  string[] private deltaLexiqueuetwo;
 
-  
+  //
+  // 'cryptonautPosinaughtTransformations' are address to string maps
+  //
+
+  mapping(address => uint) private cryptonautPosinaughtTransformationAlpha;
+  mapping(address => uint) private cryptonautPosinaughtTransformationBeta;
+  mapping(address => uint) private cryptonautPosinaughtTransformationGamma;
+  mapping(address => uint) private cryptonautPosinaughtTransformationDelta;
+
+  //
+  // 'posinaughtCryptonautTransformations' are string to address maps
+  //
+
+  mapping(uint => address) private posinaughtCryptonautTransformationAlpha;
+  mapping(uint => address) private posinaughtCryptonautTransformationBeta;
+  mapping(uint => address) private posinaughtCryptonautTransformationGamma;
+  mapping(uint => address) private posinaughtCryptonautTransformationDelta;
 
   //
   // Metaverse engine storage mutation functions follow
   //
 
-
-
-
-
-
+  
 
   constructor() payable {
   }
 
   // to support receiving ETH by default
   receive() external payable {}
+
   fallback() external payable {}
 }
